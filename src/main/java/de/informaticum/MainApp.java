ee91105804ede14b27591ff5e8366f08288ec0a0
stefan <stefan@greyhair.lan>
2020-02-03 17:22:10 +0100

    Miminale Spring-Boot-Application (mit späteren Web-Endpunkten)

---
+++ src/main/java/de/informaticum/MainApp.java
@@ -0,0 +1,13 @@
+package de.informaticum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApp {
+
+    public static final void main(final String[] args) {
+        SpringApplication.run(MainApp.class, args);
+    }
+
+}
